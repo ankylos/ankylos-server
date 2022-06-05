dab84fce8418ac4ab0ee7db7e8b28f5b3f04ae85
lambrosopos <lambrosopos@gmail.com>
2022-06-06 01:26:00 +0900

    add type hints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,16 @@
 import pytest
 from web import create_app
+from flask.testing import FlaskClient
+from flask.app import Flask
 
 # pytest-flask module for easy app testing
 @pytest.fixture
-def app():
+def app() -> Flask:
     from config import TestingConfig
     app = create_app(TestingConfig)
     return app
 
 @pytest.fixture
-def client(app):
+def client(app) -> FlaskClient:
     with app.test_client() as client:
-        yield client
+        return client
--- tests/views/test_frontend.py
+++ tests/views/test_frontend.py
@@ -1,8 +1,10 @@
 from loguru import logger
 from bs4 import BeautifulSoup
+from flask import url_for
+from flask.testing import FlaskClient
 
 
-def test_frontend_index_page_get_method(client):
+def test_frontend_index_page_get_method(client: FlaskClient) -> None:
     """
     Testing for basic index search page
 
@@ -10,7 +12,7 @@ def test_frontend_index_page_get_method(client):
         - get request returning basic html with single search bar
         - has a form with button for submit
     """
-    response = client.get("/")
+    response = client.get(url_for('frontend.index'))
 
     soup = BeautifulSoup(response.text, "html.parser")
 
@@ -24,17 +26,35 @@ def test_frontend_index_page_get_method(client):
     # Check for basic form and search button
     form = soup.find("form")
     assert form is not None
-    assert form.button is not None
+    assert form.find('button') is not None
+
+def test_index_search_button_links_to_search_page(client: FlaskClient) -> None:
+    """
+    Test for index page search button link
+
+    Testing for:
+     - search button link
+    """
+    response = client.get(url_for('frontend.index'))
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    src_btn = soup.select('button#search')[0]
+    assert src_btn is not None
+
+    form = soup.find('form')
+    assert form is not None
+    assert form.attrs['action'] == url_for('frontend.search')
 
 
-def test_frontend_search_page_get_method(client):
+
+def test_frontend_search_page_get_method(client: FlaskClient) -> None:
     """
     Test for search results page when retrieving search results
 
     Testing for:
-        - get method without params
+     - get method without params
     """
-    response = client.get("/search")
+    response = client.get(url_for('frontend.search'))
 
     assert response.status == "302 FOUND"
 
@@ -43,7 +63,7 @@ def test_frontend_search_page_get_method(client):
     assert soup.find("html") is not None
 
 
-def test_frontend_search_page_spongebob(client):
+def test_frontend_search_page_spongebob(client: FlaskClient) -> None:
     """
     Test for search term response with frontend search page
 
@@ -52,10 +72,10 @@ def test_frontend_search_page_spongebob(client):
         - returning search results
         - correct search results with given search term
     """
-    response = client.get('/search?q=spongebob')
+    response = client.get(url_for('frontend.search', q='spongebob'))
 
     assert response.status == "200 OK"
 
     soup = BeautifulSoup(response.text, 'html.parser')
     # Check search results aren't empty
-    assert soup.select(".search-result") is not None
+    assert len(soup.select(".search-result")) > 0
