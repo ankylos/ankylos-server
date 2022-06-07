d913dc456dc45e169f438d02cf3db6120e37750d
lambrosopos <lambrosopos@gmail.com>
2022-06-07 22:19:12 +0900

    add db init and creation with app factory

--- web/__init__.py
+++ web/__init__.py
@@ -5,6 +5,10 @@ def create_app(config: object):
     app = Flask(__name__)
     app.config.from_object(config)
 
+    from web.models import db
+    with app.app_context():
+        db.create_all()
+
     from web.views.frontend import frontend
 
     app.register_blueprint(frontend)
