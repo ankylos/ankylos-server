b570e95631e6aa59243895d8f2b31004d7e7c6ca
lambrosopos <lambrosopos@gmail.com>
2022-06-11 00:02:28 +0900

    search with given search term q and filter through pages table with name

--- web/views/frontend.py
+++ web/views/frontend.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, column
 from flask import request, Blueprint, render_template, url_for, redirect
 from web.models.pages import Pages
 from web.database import db_session
@@ -18,9 +18,12 @@ def search():
     if not args.get("q", default="", type=str):
         return redirect(url_for("frontend.index"))
 
+    search_term = args.get("q")
     # Handle get search term
-    stmt = select(Pages).where(Pages.name == args.get("q"))
-    results = db_session.execute(stmt)
+    # stmt = select(Pages).where(Pages.name == args.get("q"))
+    # results = db_session.execute(stmt)
+    sql_q = db_session.query(Pages).filter(Pages.name.like("%{}%".format(search_term)))
+    results = [_.name for _ in sql_q.all()]
 
     # return results
     return render_template("search.html", q_results=results)
